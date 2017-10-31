0e6fe4f1bcc9600a97e0bddabc8931db71273a84
Pete Bennett <petebennett@petes-air>
2017-10-31 21:35:51 +0000

    Initial Commit

---
+++ Cleanobot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cleanobot
+//
+//  Created by Pete Bennett on 31/10/2017.
+//  Copyright © 2017 Pete Bennett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
